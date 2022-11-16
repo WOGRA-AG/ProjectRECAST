feac9d5342fc71cc94d982bfff3b17b75d776567
Tim Köhler <tk@wogra.com>
2022-11-16 11:13:37 +0000

    Resolve "[SuPa-Stages] Deploy WebApp"

--- apps/recast/src/app/app-routing.module.ts
+++ apps/recast/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthComponent} from './user/auth/auth.component';
-import {ProfileComponent} from './user/account/profile.component';
+import {ProfileComponent} from './user/profile/profile.component';
 import {PageNotFoundComponent} from './templates/page-not-found/page-not-found.component';
 import {AuthGuard} from './user/guards/auth.guard';
 
