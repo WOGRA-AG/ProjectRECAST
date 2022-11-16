c92169f3782014502f93b4f1caf1bbd6f3b4e76c
Tim Köhler <tk@wogra.com>
2022-11-16 12:59:27 +0100

    add cypress testing

---
+++ apps/recast/cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'https://testing.os4ml.wogra.com',
+    defaultCommandTimeout: 30000,
+    experimentalStudio: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
