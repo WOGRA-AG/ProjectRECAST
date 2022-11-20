a61676b3228cc615b4af6a5c8ca949822a6f9eb5
Tim Köhler <tk@wogra.com>
2022-11-20 15:18:45 +0100

    example style form-field-input

--- apps/recast/src/app/design/design.module.ts
+++ apps/recast/src/app/design/design.module.ts
@@ -5,26 +5,41 @@ import { ButtonFilledComponent } from './components/atoms/button-filled/button-f
 import { ButtonUnfilledComponent } from './components/atoms/button-unfilled/button-unfilled.component';
 import { IconButtonFilledComponent } from './components/molecules/icon-button-filled/icon-button-filled.component';
 import { DemoComponent } from './demo/demo.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TextInputFieldComponent} from './components/molecules/text-input-field/text-input-field.component';
+import {LogoutButtonComponent} from './components/molecules/logout-button/logout-button.component';
+import {SubmitButtonComponent} from './components/molecules/submit-button/submit-button.component';
 import { IconButtonUnfilledComponent } from './components/molecules/icon-button-unfilled/icon-button-unfilled.component';
+
+const COMPONENTS = [
+  DemoComponent,
+  ButtonFilledComponent,
+  ButtonUnfilledComponent,
+  IconButtonFilledComponent,
+  IconButtonUnfilledComponent,
+  TextInputFieldComponent,
+  LogoutButtonComponent,
+  SubmitButtonComponent,
+]
 
 @NgModule({
   declarations: [
-    DemoComponent,
-    ButtonFilledComponent,
-    ButtonUnfilledComponent,
-    IconButtonFilledComponent,
-    IconButtonUnfilledComponent
+    ...COMPONENTS,
   ],
   exports: [
+    ...COMPONENTS,
     MaterialModule,
     ButtonFilledComponent,
     ButtonUnfilledComponent,
     IconButtonFilledComponent,
-    IconButtonUnfilledComponent
+    FormsModule,
+    ReactiveFormsModule,
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class DesignModule { }
