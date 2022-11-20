6069e163ecbfd259a50ab76ed5ab7852ba4a5c23
Tim Köhler <tk@wogra.com>
2022-11-20 13:17:21 +0100

    text custom form-group-items

---
+++ apps/recast/src/app/components/molecules/logout-button/logout-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-button',
+  templateUrl: './logout-button.component.html',
+  styleUrls: ['./logout-button.component.scss']
+})
+export class LogoutButtonComponent {
+
+}
