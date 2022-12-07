562f6945241b95dc39913363872f7a21c649769e
sarah <sarah.fischer@wogra.com>
2022-12-07 15:49:42 +0100

    42 impl stepper

---
+++ apps/recast/src/app/design/components/molecules/stepper/stepper.component.ts
@@ -0,0 +1,17 @@
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { MatStepper } from '@angular/material/stepper';
+
+@Component({
+  selector: 'app-stepper',
+  templateUrl: './stepper.component.html',
+})
+export class StepperComponent implements AfterViewInit {
+  @Input() steps: string[] = [];
+  @Output() stepChanged: EventEmitter<number> = new EventEmitter<number>();
+
+  @ViewChild(MatStepper) stepper: MatStepper | undefined;
+
+  ngAfterViewInit() {
+    this.stepper!._getIndicatorType = () => 'number';
+  }
+}
--- apps/recast/src/app/templates/process-overview/process-overview.component.ts
+++ apps/recast/src/app/templates/process-overview/process-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Process, Step } from 'build/openapi/recast';
 import { concatMap, filter, map, Observable } from 'rxjs';
@@ -6,6 +6,8 @@ import { Breadcrumb } from 'src/app/design/components/molecules/breadcrumb/bread
 import { TableColumn } from 'src/app/design/components/organisms/table/table.component';
 import { ElementFacadeService } from 'src/app/services/element-facade.service';
 import { ProcessFacadeService } from 'src/app/services/process-facade.service';
+import { MatStepper } from '@angular/material/stepper';
+import { StepFacadeService } from 'src/app/services/step-facade.service';
 
 @Component({
   selector: 'app-process-overview',
@@ -13,10 +15,14 @@ import { ProcessFacadeService } from 'src/app/services/process-facade.service';
   styleUrls: ['./process-overview.component.scss'],
 })
 export class ProcessOverviewComponent {
+  @ViewChild(MatStepper) stepper: MatStepper | undefined;
+
   public title = '';
   public processId: number | undefined;
   public stepId: number = 1; // TODO
   public breadcrumbs: Breadcrumb[] = [];
+  public steps: Step[] = [];
+  public stepTitles: string[] = [];
   public dataColumns: TableColumn[] = [
     { key: 'name', label: 'Title', type: 'text', required: true },
     { key: 'isEdit', label: '', type: 'isEdit' },
@@ -27,15 +33,16 @@ export class ProcessOverviewComponent {
   constructor(
     public readonly processService: ProcessFacadeService,
     public readonly elementService: ElementFacadeService,
+    public readonly stepService: StepFacadeService,
     public route: ActivatedRoute
   ) {
     this.tableData$ = elementService.elements$;
-    route.paramMap
-      .pipe(
-        filter(param => !!param.get('id')),
-        map((param, index) => +param.get('id')!),
-        concatMap(id => this.processService.processById$(id))
-      )
+    const processId$ = route.paramMap.pipe(
+      filter(param => !!param.get('id')),
+      map((param, index) => +param.get('id')!),
+    );
+
+    processId$.pipe(concatMap(id => this.processService.processById$(id)))
       .subscribe(process => {
         this.processId = process.id;
         this.title = process.name!;
@@ -43,7 +50,17 @@ export class ProcessOverviewComponent {
           { label: $localize`:@@header.overview:Overview`, link: '/overview' },
           { label: this.title },
         ];
-      });
+    });
+
+    processId$.pipe(concatMap(id => this.stepService.stepsByProcessId$(id)))
+      .subscribe(steps => {
+        this.steps = steps;
+        this.stepTitles = steps.map(step => step.name!);
+    });
+  }
+
+  public changeContent(id: number) {
+    //TODO
   }
 
   public deleteTableRow(element: Process | Element | Step): void {
