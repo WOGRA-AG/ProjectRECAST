2f4a1cebb17f5742a00923246afef1a36a338f12
Thomas Fraunholz <tf@wogra.com>
2023-01-31 17:49:12 +0100

    made it up to a watcher

--- apps/device/src/index.ts
+++ apps/device/src/index.ts
@@ -1,6 +1,6 @@
 import { RecastClient } from './client'
-import { RealtimeClient } from '@supabase/realtime-js'
-
+import { mkdirp } from 'mkdirp'
+import * as chokidar from 'chokidar';
 
 const supabaseurl = 'fbrhcyxrfnfojfuioexq.supabase.co'
 const supabasekey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZicmhjeXhyZm5mb2pmdWlvZXhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5ODkzNzksImV4cCI6MTk4MzU2NTM3OX0.B11sNHYYnEskYELwPz6LkKnl2z2sK3th5E5K9lLHlNU'
@@ -9,15 +9,44 @@ const password = 'example_js-password'
 
 let client = new RecastClient('https://' + supabaseurl, supabasekey, email, password)
 
-async function get_upload(client: RecastClient): Promise<{ bucket: string, prefix: string }> {
-  //SELECT bucket, prefix FROM upload WHERE (status is true AND owner_id = '0b027f1a-f439-4953-9089ud1a2144b7d88' )
-  let { data, error } = await client.supabase.from('upload').select('bucket, prefix').is('status', true)
-  let bucket: string = data && data[0].bucket;
-  let prefix: string = data && data[0].prefix;
-  return { bucket , prefix }
+async function get_upload(client: RecastClient): Promise<{ bucket: string, prefix: string } | null> {
+  let { data, error } = await client.supabase.from('upload').select('bucket, prefix').is('status', true);
+
+  if (data === null) {
+    console.log(`No active upload!`);
+    return null;
+  } 
+  else {
+    let bucket: string = data && data[0].bucket;
+    console.log(`Bucket "${bucket}".`);
+    let prefix: string = data && data[0].prefix;
+    console.log(`Prefix "${prefix}".`);
+    return { bucket , prefix };
+  }
 }
 
-get_upload(client).then(data => console.log(data.bucket))
+async function create_folder(folderPath: string): Promise<string> {
+  let made = mkdirp(folderPath);  
+  folderPath = typeof made == 'string' ? made : folderPath;
+  return folderPath
+}
+
+async function inital_call(client: RecastClient): Promise<void> {
+  const data = await get_upload(client);
+  if (data !== null) {
+    const folderPath = await create_folder('./data/' + data.prefix);
+    const watcher = chokidar.watch(folderPath, {
+      persistent: true,
+      ignoreInitial: true
+    });
+    watcher
+      .on('add', (path) => console.log(`File ${path} has been added`))
+      .on('change', (path) => console.log(`File ${path} has been changed`))
+      .on('unlink', (path) => console.log(`File ${path} has been removed`));
+  }
+}
+
+inital_call(client)
 
 const fileUpload = client.supabase.channel('custom-all-channel')
 .on(
