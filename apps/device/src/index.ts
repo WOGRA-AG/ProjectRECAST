50a4f6c4f58da6984c4c1a46c49706754f8fcaef
Thomas Fraunholz <tf@wogra.com>
2023-01-31 18:07:40 +0100

    made a watcher class

--- apps/device/src/index.ts
+++ apps/device/src/index.ts
@@ -1,13 +1,14 @@
 import { RecastClient } from './client'
 import { mkdirp } from 'mkdirp'
-import * as chokidar from 'chokidar';
+import { Watcher } from './watcher'
 
 const supabaseurl = 'fbrhcyxrfnfojfuioexq.supabase.co'
 const supabasekey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZicmhjeXhyZm5mb2pmdWlvZXhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5ODkzNzksImV4cCI6MTk4MzU2NTM3OX0.B11sNHYYnEskYELwPz6LkKnl2z2sK3th5E5K9lLHlNU'
 const email = 'example_js@email.com'
 const password = 'example_js-password'
 
-let client = new RecastClient('https://' + supabaseurl, supabasekey, email, password)
+let client: RecastClient = new RecastClient('https://' + supabaseurl, supabasekey, email, password)
+let watcher: Watcher = new Watcher();
 
 async function get_upload(client: RecastClient): Promise<{ bucket: string, prefix: string } | null> {
   let { data, error } = await client.supabase.from('upload').select('bucket, prefix').is('status', true);
@@ -35,14 +36,7 @@ async function inital_call(client: RecastClient): Promise<void> {
   const data = await get_upload(client);
   if (data !== null) {
     const folderPath = await create_folder('./data/' + data.prefix);
-    const watcher = chokidar.watch(folderPath, {
-      persistent: true,
-      ignoreInitial: true
-    });
-    watcher
-      .on('add', (path) => console.log(`File ${path} has been added`))
-      .on('change', (path) => console.log(`File ${path} has been changed`))
-      .on('unlink', (path) => console.log(`File ${path} has been removed`));
+    watcher.start(folderPath);
   }
 }
 
---
+++ apps/device/src/watcher.ts
@@ -0,0 +1,21 @@
+import * as chokidar from 'chokidar';
+
+export class Watcher {
+  private chokidarWatcher: chokidar.FSWatcher | undefined;
+
+  start(path: string) {
+    this.chokidarWatcher = chokidar.watch(path, {});
+
+    this.chokidarWatcher
+      .on('add', (path: string) => console.log(`File ${path} has been added`))
+      .on('change', (path: string) => console.log(`File ${path} has been changed`))
+      .on('unlink', (path: string) => console.log(`File ${path} has been removed`));
+  }
+
+  stop() {
+    if (this.chokidarWatcher) {
+      this.chokidarWatcher.close();
+    }
+  }
+}
+
